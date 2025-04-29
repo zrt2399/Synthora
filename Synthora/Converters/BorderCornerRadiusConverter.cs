e2f3d2a7f59440c250d5634cc100e1789b4e90c4
TheShy <TheShy@qq.com>
2025-04-29 19:44:56 +0800

    Update BorderCornerRadiusConverter to support innerThickness

--- Synthora/Converters/BorderCornerRadiusConverter.cs
+++ Synthora/Converters/BorderCornerRadiusConverter.cs
@@ -22,7 +22,7 @@ namespace Synthora.Converters
     {
         public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
         {
-            if (values.Count == 2 && values[0] is Thickness thickness && values[1] is CornerRadius cornerRadius)
+            if (values.Count >= 2 && values[0] is Thickness thickness && values[1] is CornerRadius cornerRadius)
             {
                 CornerRadiusType cornerRadiusType = CornerRadiusType.None;
                 if (parameter is CornerRadiusType type)
@@ -35,11 +35,27 @@ namespace Synthora.Converters
                 }
                 if (cornerRadiusType != CornerRadiusType.None)
                 {
+                    double innerLeft = 0, innerRight = 0, innerTop = 0, innerBottom = 0;
+                    if (values.Count == 3)
+                    {
+                        if (values[2] is Thickness innerThickness)
+                        {
+                            innerLeft = innerThickness.Left;
+                            innerRight = innerThickness.Right;
+                            innerTop = innerThickness.Top;
+                            innerBottom = innerThickness.Bottom;
+                        }
+                        else if (double.TryParse(values[2]?.ToString(), out var result))
+                        {
+                            innerLeft = innerRight = innerTop = innerBottom = result;
+                        }
+                    }
+
                     return new CornerRadius(
-                        cornerRadiusType.HasFlag(CornerRadiusType.TopLeft) ? ConversionUtils.CalcInnerRadius(cornerRadius.TopLeft, thickness.Left) : 0,
-                        cornerRadiusType.HasFlag(CornerRadiusType.TopRight) ? ConversionUtils.CalcInnerRadius(cornerRadius.TopRight, thickness.Top) : 0,
-                        cornerRadiusType.HasFlag(CornerRadiusType.BottomRight) ? ConversionUtils.CalcInnerRadius(cornerRadius.BottomRight, thickness.Right) : 0,
-                        cornerRadiusType.HasFlag(CornerRadiusType.BottomLeft) ? ConversionUtils.CalcInnerRadius(cornerRadius.BottomLeft, thickness.Bottom) : 0);
+                        cornerRadiusType.HasFlag(CornerRadiusType.TopLeft) ? ConversionUtils.CalcInnerRadius(cornerRadius.TopLeft, thickness.Left, innerLeft) : 0,
+                        cornerRadiusType.HasFlag(CornerRadiusType.TopRight) ? ConversionUtils.CalcInnerRadius(cornerRadius.TopRight, thickness.Top, innerTop) : 0,
+                        cornerRadiusType.HasFlag(CornerRadiusType.BottomRight) ? ConversionUtils.CalcInnerRadius(cornerRadius.BottomRight, thickness.Right, innerRight) : 0,
+                        cornerRadiusType.HasFlag(CornerRadiusType.BottomLeft) ? ConversionUtils.CalcInnerRadius(cornerRadius.BottomLeft, thickness.Bottom, innerBottom) : 0);
                 }
                 return new CornerRadius(0);
             }
