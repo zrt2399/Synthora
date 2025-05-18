7be875027b636e3e67e47750e6794ce45d1f9788
Yoyo <zrt2399@163.com>
2025-05-18 22:24:46 +0800

    MessageTip.ShowOK method rename

---
+++ Synthora/Controls/TipIcon.cs
@@ -0,0 +1,85 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using Synthora.Messaging;
+using Synthora.Utils;
+
+namespace Synthora.Controls
+{
+    internal class IconBase : TemplatedControl
+    {
+        public static readonly StyledProperty<IconType> IconTypeProperty =
+          AvaloniaProperty.Register<IconBase, IconType>(nameof(IconType), IconType.Information);
+
+        static IconBase()
+        {
+            AffectsRender<IconBase>(IconTypeProperty);
+        }
+
+        public IconType IconType
+        {
+            get => GetValue(IconTypeProperty);
+            set => SetValue(IconTypeProperty, value);
+        }
+    }
+
+    internal class TipIcon : IconBase
+    {
+
+    }
+
+    internal class StatusIcon : IconBase
+    {
+        public StatusIcon()
+        {
+            Height = 20;
+            Width = 20;
+        }
+
+        private double ActualHeight => Bounds.Height;
+        private double ActualWidth => Bounds.Width;
+
+        public override void Render(DrawingContext drawingContext)
+        {
+            if (IconType == IconType.Information)
+            {
+                var background = new SolidColorBrush(Color.FromRgb(103, 148, 186));
+                var pen = new Pen(background, 2);
+                drawingContext.DrawEllipse(background, pen, new Point(ActualHeight / 2, ActualWidth / 2), ActualHeight / 2 - 1, ActualWidth / 2 - 1);
+
+                var exclamationPen = new Pen(new SolidColorBrush(Color.FromRgb(245, 245, 245)), 2);
+                drawingContext.DrawLine(exclamationPen, new Point(10, 4), new Point(10, 6));
+                drawingContext.DrawLine(exclamationPen, new Point(10, 8), new Point(10, 16));
+            }
+            else if (IconType == IconType.Success)
+            {
+                StreamGeometry geometry = new StreamGeometry();
+                using (StreamGeometryContext ctx = geometry.Open())
+                {
+                    ctx.BeginFigure(new Point(1.4, 10), false);
+                    ctx.LineTo(new Point(8, 16));
+                    ctx.LineTo(new Point(18.4, 2));
+                }
+                var background = new SolidColorBrush(Color.FromRgb(110, 190, 40));
+                var pen = new Pen(background, 4);
+                drawingContext.DrawGeometry(background, pen, geometry);
+            }
+            else if (IconType == IconType.Warning)
+            {
+                var points = new[] { new Point(10, 1), new Point(19.2, 18.6), new Point(0.8, 18.6) };
+                var background = new SolidColorBrush(Color.FromRgb(220, 155, 40));
+                var pen = new Pen(background, 2) { LineJoin = PenLineJoin.Bevel };
+                drawingContext.DrawPolygon(background, pen, points);
+                var exclamationPen = new Pen(new SolidColorBrush(Color.FromRgb(251, 245, 233)), 2);
+                drawingContext.DrawLine(exclamationPen, new Point(10.5, 6), new Point(10.5, 14));
+                drawingContext.DrawLine(exclamationPen, new Point(10.5, 16), new Point(10.5, 18));
+            }
+            else if (IconType == IconType.Error)
+            {
+                var pen = new Pen(new SolidColorBrush(Color.FromRgb(230, 80, 80)), 4);
+                drawingContext.DrawLine(pen, new Point(2, 2), new Point(18, 18));
+                drawingContext.DrawLine(pen, new Point(18, 2), new Point(2, 18));
+            }
+        }
+    }
+}
--- Synthora/Messaging/MessageTip.cs
+++ Synthora/Messaging/MessageTip.cs
@@ -7,14 +7,14 @@ using Avalonia.Controls.Primitives;
 using Avalonia.Layout;
 using Avalonia.Media;
 using Avalonia.Threading;
-using Synthora.Utils;
+using Synthora.Controls;
 
 namespace Synthora.Messaging
 {
     public enum IconType
     {
-        Info,
-        OK,
+        Information,
+        Success,
         Warning,
         Error
     }
@@ -27,9 +27,9 @@ namespace Synthora.Messaging
 
         public static CornerRadius TipCornerRadius { get; set; } = new CornerRadius(4);
 
-        public static void Show(string message, int delay = Delay) => Show(message, IconType.Info, delay);
+        public static void Show(string message, int delay = Delay) => Show(message, IconType.Information, delay);
 
-        public static void ShowOK(string message, int delay = Delay) => Show(message, IconType.OK, delay);
+        public static void ShowSuccess(string message, int delay = Delay) => Show(message, IconType.Success, delay);
 
         public static void ShowWarning(string message, int delay = Delay) => Show(message, IconType.Warning, delay);
 
@@ -66,8 +66,8 @@ namespace Synthora.Messaging
 
                     var borderBrush = iconType switch
                     {
-                        IconType.Info => SolidColorBrush.Parse("#8C8C8C"),
-                        IconType.OK => SolidColorBrush.Parse("#6EBE28"),
+                        IconType.Information => SolidColorBrush.Parse("#8C8C8C"),
+                        IconType.Success => SolidColorBrush.Parse("#6EBE28"),
                         IconType.Warning => SolidColorBrush.Parse("#DC9B28"),
                         _ => SolidColorBrush.Parse("#E65050")
                     };
@@ -85,7 +85,7 @@ namespace Synthora.Messaging
 
                     textBlock.FontSize = application.Resources["FontSizeNormal"] as double? ?? 12;
                     textBlock.SetValue(Grid.ColumnProperty, 1);
-                    grid.Children.Add(new Viewbox() { Width = 20, Height = 20, Child = new TipIcon() { IconType = iconType, Margin = new Thickness(2) } });
+                    grid.Children.Add(new Viewbox() { Width = 16, Height = 16, Child = new StatusIcon() { IconType = iconType } });
                     grid.Children.Add(textBlock);
 
                     Border border = new Border();
@@ -127,75 +127,6 @@ namespace Synthora.Messaging
                 popupRoot.Background = null;
                 popupRoot.TransparencyBackgroundFallback = Brushes.Transparent;
                 popupRoot.TransparencyLevelHint = [WindowTransparencyLevel.Transparent];
-            }
-        }
-    }
-
-    public class TipIcon : Control
-    {
-        public static readonly StyledProperty<IconType> IconTypeProperty =
-            AvaloniaProperty.Register<TipIcon, IconType>(nameof(IconType), IconType.Info);
-
-        static TipIcon()
-        {
-            AffectsRender<TipIcon>(IconTypeProperty);
-        }
-
-        public TipIcon()
-        {
-            Height = 20;
-            Width = 20;
-        }
-
-        private double ActualHeight => Bounds.Height;
-        private double ActualWidth => Bounds.Width;
-
-        public IconType IconType
-        {
-            get => GetValue(IconTypeProperty);
-            set => SetValue(IconTypeProperty, value);
-        }
-
-        public override void Render(DrawingContext drawingContext)
-        {
-            if (IconType == IconType.Info)
-            {
-                var background = new SolidColorBrush(Color.FromRgb(103, 148, 186));
-                var pen = new Pen(background, 2);
-                drawingContext.DrawEllipse(background, pen, new Point(ActualHeight / 2, ActualWidth / 2), ActualHeight / 2 - 1, ActualWidth / 2 - 1);
-
-                var exclamationPen = new Pen(new SolidColorBrush(Color.FromRgb(245, 245, 245)), 2);
-                drawingContext.DrawLine(exclamationPen, new Point(10, 4), new Point(10, 6));
-                drawingContext.DrawLine(exclamationPen, new Point(10, 8), new Point(10, 16));
-            }
-            else if (IconType == IconType.OK)
-            {
-                StreamGeometry geometry = new StreamGeometry();
-                using (StreamGeometryContext ctx = geometry.Open())
-                {
-                    ctx.BeginFigure(new Point(1.4, 10), false);
-                    ctx.LineTo(new Point(8, 16));
-                    ctx.LineTo(new Point(18.4, 2));
-                }
-                var background = new SolidColorBrush(Color.FromRgb(110, 190, 40));
-                var pen = new Pen(background, 4);
-                drawingContext.DrawGeometry(background, pen, geometry);
-            }
-            else if (IconType == IconType.Warning)
-            {
-                var points = new[] { new Point(10, 1), new Point(19.2, 18.6), new Point(0.8, 18.6) };
-                var background = new SolidColorBrush(Color.FromRgb(220, 155, 40));
-                var pen = new Pen(background, 2) { LineJoin = PenLineJoin.Bevel };
-                drawingContext.DrawPolygon(background, pen, points);
-                var exclamationPen = new Pen(new SolidColorBrush(Color.FromRgb(251, 245, 233)), 2);
-                drawingContext.DrawLine(exclamationPen, new Point(10.5, 6), new Point(10.5, 14));
-                drawingContext.DrawLine(exclamationPen, new Point(10.5, 16), new Point(10.5, 18));
-            }
-            else if (IconType == IconType.Error)
-            {
-                var pen = new Pen(new SolidColorBrush(Color.FromRgb(230, 80, 80)), 4);
-                drawingContext.DrawLine(pen, new Point(2, 2), new Point(18, 18));
-                drawingContext.DrawLine(pen, new Point(18, 2), new Point(2, 18));
             }
         }
     }
