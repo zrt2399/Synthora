10b94d323c38a489b465628ce0f9e5c29131a2db
Yoyo <zrt2399@163.com>
2025-06-01 03:46:32 +0800

    feat: Add AvaloniaPropertyUtils.

---
+++ Synthora/Utils/AvaloniaPropertyUtils.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+
+namespace Synthora.Utils
+{
+    internal static class AvaloniaPropertyUtils
+    {
+        public static void SetValue<T>(this AvaloniaProperty<T> property, T value, params AvaloniaObject?[] objects)
+        {
+            foreach (var obj in objects)
+            {
+                obj?.SetValue(property, value);
+            }
+        }
+
+        public static void SetCurrentValue<T>(this AvaloniaProperty<T> property, T value, params AvaloniaObject?[] objects)
+        {
+            foreach (var obj in objects)
+            {
+                obj?.SetCurrentValue(property, value);
+            }
+        }
+    }
+}
