4dbad506760814912a6cbf37d93ec82894443961
Yoyo <zrt2399@163.com>
2026-05-09 03:27:59 +0800

    feat(scrollBar): Add ScrollBarAttach.

---
+++ Synthora/Attaches/ScrollBarAttach.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Input;
+
+namespace Synthora.Attaches
+{
+    public class ScrollBarAttach
+    {
+        public static readonly AttachedProperty<bool> InnerOffsetEnabledProperty =
+            AvaloniaProperty.RegisterAttached<ScrollBarAttach, InputElement, bool>("InnerOffsetEnabled", defaultValue: true);
+
+        public static bool GetInnerOffsetEnabled(InputElement obj) => obj.GetValue(InnerOffsetEnabledProperty);
+        public static void SetInnerOffsetEnabled(InputElement obj, bool value) => obj.SetValue(InnerOffsetEnabledProperty, value);
+    }
+}
