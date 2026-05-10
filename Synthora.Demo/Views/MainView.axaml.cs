43756c06c8fa7af3f2807c7933c2cde71da9b2a8
Yoyo <zrt2399@163.com>
2026-05-11 07:53:43 +0800

    feat(navigation): Implement NavigationPage with command buttons for navigation actions

--- Synthora.Demo/Views/MainView.axaml.cs
+++ Synthora.Demo/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Synthora.Demo.Views;
 
@@ -14,4 +15,30 @@ public partial class MainView : UserControl
     {
         e.Row.Header = e.Row.Index + 1;
     }
+    
+    private void PushAsync(object? sender, RoutedEventArgs e)
+    {
+        navigationPage.PushAsync(new ContentPage(){Header = "PushAsyncHeader",Content = new TextBlock(){Text = "PushAsyncContent"}}); 
+    }     
+    private void PopAsync(object? sender, RoutedEventArgs e)
+    {
+        navigationPage.PopAsync();  
+    } 
+    private void PopToRootAsync(object? sender, RoutedEventArgs e)
+    {
+        navigationPage.PopToRootAsync();  
+    }  
+    
+    private void PushModalAsync(object? sender, RoutedEventArgs e)
+    {
+        navigationPage.PushModalAsync(new ContentPage(){Header = "PushAsyncHeader",Content = new TextBlock(){Text = "PushModalAsyncContent"}}); 
+    }     
+    private void PopModalAsync(object? sender, RoutedEventArgs e)
+    {
+        navigationPage.PopModalAsync();  
+    }     
+    private void PopAllModalsAsync(object? sender, RoutedEventArgs e)
+    {
+        navigationPage.PopAllModalsAsync(); 
+    }   
 }
