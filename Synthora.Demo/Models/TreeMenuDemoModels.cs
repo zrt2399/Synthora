775bbe12b81582672b0f993d7d7548cd9d9e7fd6
Yoyo <zrt2399@163.com>
2026-05-19 12:16:43 +0800

    chore: format code

--- Synthora.Demo/Models/TreeMenuDemoModels.cs
+++ Synthora.Demo/Models/TreeMenuDemoModels.cs
@@ -10,14 +10,15 @@ namespace Synthora.Demo.Models
         {
             Title = GetType().Name.Replace("ViewModel", "");
         }
+        
+        [ObservableProperty]
+        public partial MaterialIconKind IconKind { get; set; }
 
         [ObservableProperty]
         public partial string Title { get; set; }
 
         [ObservableProperty]
         public partial string Description { get; set; } = string.Empty;
-        [ObservableProperty]
-        public partial MaterialIconKind IconKind { get; set; }
 
         [ObservableProperty]
         public partial ObservableCollection<TreeMenuDemoItem> Children { get; set; } = [];
