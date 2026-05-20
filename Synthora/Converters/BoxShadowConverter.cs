13807585090648825171c5814a4b57832ceb3bbe
Yoyo <zrt2399@163.com>
2026-05-20 11:46:19 +0800

    feat: update BoxShadow parameters for Button styles

--- Synthora/Converters/BoxShadowConverter.cs
+++ Synthora/Converters/BoxShadowConverter.cs
@@ -7,7 +7,7 @@ namespace Synthora.Converters
 {
     public class BoxShadowConverter : IValueConverter
     {
-        public const string DefaultParameter = "0 0 8 0 #00000000";
+        public const string ButtonPointerOverParameter = "0 0 6 0 #00000000";
 
         public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
