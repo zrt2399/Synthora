d1e6950f14369810e2af67cc00f149535f0fa33a
Yoyo <zrt2399@163.com>
2026-06-01 11:07:25 +0800

    chore: format code

--- Synthora/Converters/SynthoraConverters.cs
+++ Synthora/Converters/SynthoraConverters.cs
@@ -6,17 +6,17 @@
         public static NotEqualsConverter NotEqual { get; } = new NotEqualsConverter();
         public static NullOrEmptyToBoolConverter IsNullOrEmpty { get; } = new NullOrEmptyToBoolConverter();
         public static NotNullOrEmptyToBoolConverter IsNotNullOrEmpty { get; } = new NotNullOrEmptyToBoolConverter();
-        public static EnumDescriptionConverter EnumDescriptionConverter { get; } = new EnumDescriptionConverter();
         public static HasFlagConverter HasFlag { get; } = new HasFlagConverter();
         public static IsZeroConverter IsZero { get; } = new IsZeroConverter();
         public static IsNotZeroConverter IsNotZero { get; } = new IsNotZeroConverter();
         public static BoxShadowConverter BoxShadowConverter { get; } = new BoxShadowConverter();
         public static ContrastColorConverter ContrastColorConverter { get; } = new ContrastColorConverter();
+        public static EnumDescriptionConverter EnumDescriptionConverter { get; } = new EnumDescriptionConverter();
         public static BorderCornerRadiusConverter BorderCornerRadiusConverter { get; } = new BorderCornerRadiusConverter();
-        public static SolidColorBrushToColorConverter SolidColorBrushToColor { get; } = new SolidColorBrushToColorConverter();
         public static DoubleToGridLengthConverter DoubleToGridLengthConverter { get; } = new DoubleToGridLengthConverter();
         public static DoubleToCornerRadiusConverter DoubleToCornerRadiusConverter { get; } = new DoubleToCornerRadiusConverter();
         public static ThemeModeToPathDataConverter ThemeModeToPathDataConverter { get; } = new ThemeModeToPathDataConverter();
         public static DensityStyleToPathDataConverter DensityStyleToPathDataConverter { get; } = new DensityStyleToPathDataConverter();
+        public static SolidColorBrushToColorConverter SolidColorBrushToColorConverter { get; } = new SolidColorBrushToColorConverter();
     }
 }
