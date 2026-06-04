aee865f7b77f41430f445df306944a478e57aa3f
Yoyo <zrt2399@163.com>
2026-06-04 15:59:22 +0800

    feat: enhance TabControl and TabbedPage

--- Synthora/Attaches/TabControlAttach.cs
+++ Synthora/Attaches/TabControlAttach.cs
@@ -7,21 +7,21 @@ namespace Synthora.Attaches
     public class TabControlAttach
     {
         public static readonly AttachedProperty<HorizontalAlignment> HorizontalHeaderAlignmentProperty =
-            AvaloniaProperty.RegisterAttached<TabControlAttach, TabControl, HorizontalAlignment>("HorizontalHeaderAlignment");
+            AvaloniaProperty.RegisterAttached<TabControlAttach, Control, HorizontalAlignment>("HorizontalHeaderAlignment", defaultValue: HorizontalAlignment.Left);
 
         public static readonly AttachedProperty<VerticalAlignment> VerticalHeaderAlignmentProperty =
-            AvaloniaProperty.RegisterAttached<TabControlAttach, TabControl, VerticalAlignment>("VerticalHeaderAlignment");
+            AvaloniaProperty.RegisterAttached<TabControlAttach, Control, VerticalAlignment>("VerticalHeaderAlignment", defaultValue: VerticalAlignment.Top);
 
         public static readonly AttachedProperty<bool> ShowHeaderSeparatorProperty =
-            AvaloniaProperty.RegisterAttached<TabControlAttach, TabControl, bool>("ShowHeaderSeparator", defaultValue: true);
+            AvaloniaProperty.RegisterAttached<TabControlAttach, Control, bool>("ShowHeaderSeparator", defaultValue: true);
 
-        public static HorizontalAlignment GetHorizontalHeaderAlignment(TabControl obj) => obj.GetValue(HorizontalHeaderAlignmentProperty);
-        public static void SetHorizontalHeaderAlignment(TabControl obj, HorizontalAlignment value) => obj.SetValue(HorizontalHeaderAlignmentProperty, value);
+        public static HorizontalAlignment GetHorizontalHeaderAlignment(Control obj) => obj.GetValue(HorizontalHeaderAlignmentProperty);
+        public static void SetHorizontalHeaderAlignment(Control obj, HorizontalAlignment value) => obj.SetValue(HorizontalHeaderAlignmentProperty, value);
 
-        public static VerticalAlignment GetVerticalHeaderAlignment(TabControl obj) => obj.GetValue(VerticalHeaderAlignmentProperty);
-        public static void SetVerticalHeaderAlignment(TabControl obj, VerticalAlignment value) => obj.SetValue(VerticalHeaderAlignmentProperty, value);
+        public static VerticalAlignment GetVerticalHeaderAlignment(Control obj) => obj.GetValue(VerticalHeaderAlignmentProperty);
+        public static void SetVerticalHeaderAlignment(Control obj, VerticalAlignment value) => obj.SetValue(VerticalHeaderAlignmentProperty, value);
 
-        public static bool GetShowHeaderSeparator(TabControl obj) => obj.GetValue(ShowHeaderSeparatorProperty);
-        public static void SetShowHeaderSeparator(TabControl obj, bool value) => obj.SetValue(ShowHeaderSeparatorProperty, value);
+        public static bool GetShowHeaderSeparator(Control obj) => obj.GetValue(ShowHeaderSeparatorProperty);
+        public static void SetShowHeaderSeparator(Control obj, bool value) => obj.SetValue(ShowHeaderSeparatorProperty, value);
     }
 }
