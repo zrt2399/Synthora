5a96bb8d298989aa37345d85b127f61e395338df
Yoyo <zrt2399@163.com>
2026-06-06 16:31:28 +0800

    style: improve SimpleMenuScrollViewer style

--- Synthora.Demo/ViewModels/MenuViewModel.cs
+++ Synthora.Demo/ViewModels/MenuViewModel.cs
@@ -1,14 +1,25 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Material.Icons;
 using Synthora.Demo.Models;
 
 namespace Synthora.Demo.ViewModels
 {
-    public class MenuViewModel: TreeMenuDemoItem
+    public class MenuViewModel : TreeMenuDemoItem
     {
         public MenuViewModel()
         {
             IconKind = MaterialIconKind.HamburgerMenu;
             Description = "Menu, MenuItem, MenuFlyout, ContextMenu, ToolTip, CommandBar, CommandBarButton, CommandBarToggleButton";
-        } 
+
+            var items = new List<string>();
+            for (int i = 1; i <= 80; i++)
+            {
+                items.Add($"Menu Item {i}");
+            }
+            MenuItems = new ObservableCollection<string>(items);
+        }
+
+        public ObservableCollection<string> MenuItems { get; }
     }
 }
