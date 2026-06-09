c7ea388e281286825a2bb8ddcea197b92bfbc75e
Yoyo <zrt2399@163.com>
2026-06-09 20:46:06 +0800

    feat: add ShowShadowOnInteraction property to ShadowChrome

--- Synthora/Controls/ShadowChrome.cs
+++ Synthora/Controls/ShadowChrome.cs
@@ -8,11 +8,29 @@ namespace Synthora.Controls
     {
         public static readonly StyledProperty<BoxShadows> BoxShadowProperty = 
             AvaloniaProperty.Register<ShadowChrome, BoxShadows>(nameof(BoxShadow));
- 
+         
+        public static readonly StyledProperty<bool> ShowShadowOnInteractionProperty = 
+            AvaloniaProperty.Register<ShadowChrome, bool>(nameof(ShowShadowOnInteraction));
+        
+        public static readonly StyledProperty<BoxShadows> InteractionBoxShadowProperty = 
+            AvaloniaProperty.Register<ShadowChrome, BoxShadows>(nameof(InteractionBoxShadow));
+
         public BoxShadows BoxShadow
         {
             get => GetValue(BoxShadowProperty);
             set => SetValue(BoxShadowProperty, value);
+        }
+
+        public bool ShowShadowOnInteraction
+        {
+            get => GetValue(ShowShadowOnInteractionProperty);
+            set => SetValue(ShowShadowOnInteractionProperty, value);
+        }
+
+        public BoxShadows InteractionBoxShadow
+        {
+            get => GetValue(InteractionBoxShadowProperty);
+            set => SetValue(InteractionBoxShadowProperty, value);
         }
     }
 }
