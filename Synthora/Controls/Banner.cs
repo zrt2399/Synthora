d146168bcc4c014c63b274dce40e176cf0de9413
Yoyo <zrt2399@163.com>
2026-06-10 13:43:38 +0800

    feat: add Banner control with customizable icons and messages

---
+++ Synthora/Controls/Banner.cs
@@ -0,0 +1,94 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+
+namespace Synthora.Controls
+{
+    /// <summary>
+    /// Represents a banner control for displaying messages with different severity levels.
+    /// </summary>
+    [PseudoClasses(pcInformation, pcSuccess, pcWarning, pcError, pcNone)]
+    public class Banner : ContentControl
+    {
+        private const string pcNone = ":none";
+        private const string pcInformation = ":information";
+        private const string pcSuccess = ":success";
+        private const string pcWarning = ":warning";
+        private const string pcError = ":error";
+
+        /// <summary>
+        /// Defines the <see cref="IconType"/> property.
+        /// </summary>
+        public static readonly StyledProperty<IconType> IconTypeProperty =
+            AvaloniaProperty.Register<Banner, IconType>(nameof(IconType));
+
+        /// <summary>
+        /// Defines the <see cref="ShowCloseButton"/> property.
+        /// </summary>
+        public static readonly StyledProperty<bool> ShowCloseButtonProperty =
+            AvaloniaProperty.Register<Banner, bool>(nameof(ShowCloseButton), true);
+
+        /// <summary>
+        /// Defines the <see cref="IsClosed"/> property.
+        /// </summary>
+        public static readonly DirectProperty<Banner, bool> IsClosedProperty =
+            AvaloniaProperty.RegisterDirect<Banner, bool>(nameof(IsClosed), o => o.IsClosed, (o, v) => o.IsClosed = v);
+
+        private bool _isClosed;
+
+        static Banner()
+        {
+            IconTypeProperty.Changed.AddClassHandler<Banner, IconType>((s, e) => s.UpdatePseudoClasses());
+        }
+
+        /// <summary>
+        /// Gets or sets the icon type of the banner.
+        /// </summary>
+        public IconType IconType
+        {
+            get => GetValue(IconTypeProperty);
+            set => SetValue(IconTypeProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets whether to show the close button.
+        /// </summary>
+        public bool ShowCloseButton
+        {
+            get => GetValue(ShowCloseButtonProperty);
+            set => SetValue(ShowCloseButtonProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets whether the banner is closed.
+        /// </summary>
+        public bool IsClosed
+        {
+            get => _isClosed;
+            set => SetAndRaise(IsClosedProperty, ref _isClosed, value);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+
+            UpdatePseudoClasses();
+        }
+
+        private void UpdatePseudoClasses()
+        {
+            PseudoClasses.Set(pcNone, IconType == IconType.None);
+            PseudoClasses.Set(pcInformation, IconType == IconType.Information);
+            PseudoClasses.Set(pcSuccess, IconType == IconType.Success);
+            PseudoClasses.Set(pcWarning, IconType == IconType.Warning);
+            PseudoClasses.Set(pcError, IconType == IconType.Error);
+        }
+
+        public void Close()
+        {
+            IsClosed = true;
+        }
+    }
+}
