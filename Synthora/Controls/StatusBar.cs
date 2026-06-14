85cde6b2efba4d0d5d861589c6a21da50c4ed38c
Yoyo <zrt2399@163.com>
2026-06-14 08:46:56 +0800

    feat: add RunningBackground and RunningForeground properties to StatusBar

--- Synthora/Controls/StatusBar.cs
+++ Synthora/Controls/StatusBar.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace Synthora.Controls
 {
@@ -8,10 +9,28 @@ namespace Synthora.Controls
         public static readonly StyledProperty<bool> IsRunningProperty =
             AvaloniaProperty.Register<StatusBar, bool>(nameof(IsRunning));
 
+        public static readonly StyledProperty<IBrush?> RunningBackgroundProperty =
+            AvaloniaProperty.Register<StatusBar, IBrush?>(nameof(RunningBackground));
+
+        public static readonly StyledProperty<IBrush?> RunningForegroundProperty =
+            AvaloniaProperty.Register<StatusBar, IBrush?>(nameof(RunningForeground));
+
         public bool IsRunning
         {
             get => GetValue(IsRunningProperty);
             set => SetValue(IsRunningProperty, value);
+        }
+
+        public IBrush? RunningBackground
+        {
+            get => GetValue(RunningBackgroundProperty);
+            set => SetValue(RunningBackgroundProperty, value);
+        }
+        
+        public IBrush? RunningForeground
+        {
+            get => GetValue(RunningForegroundProperty);
+            set => SetValue(RunningForegroundProperty, value);
         }
     }
 }
