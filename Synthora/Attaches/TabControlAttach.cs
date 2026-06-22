cda684178a7744000145fcabe714116c9a7a4f37
Yoyo <zrt2399@163.com>
2026-06-22 19:52:39 +0800

    feat: add attached properties for HeaderCornerRadius, HeaderBorderThickness, and HeaderPadding in TabControl

--- Synthora/Attaches/TabControlAttach.cs
+++ Synthora/Attaches/TabControlAttach.cs
@@ -15,6 +15,15 @@ namespace Synthora.Attaches
         public static readonly AttachedProperty<bool> ShowHeaderSeparatorProperty =
             AvaloniaProperty.RegisterAttached<TabControlAttach, Control, bool>("ShowHeaderSeparator", defaultValue: true);
 
+        public static readonly AttachedProperty<CornerRadius> HeaderCornerRadiusProperty =
+            AvaloniaProperty.RegisterAttached<TabControlAttach, Control, CornerRadius>("HeaderCornerRadius");
+
+        public static readonly AttachedProperty<Thickness> HeaderBorderThicknessProperty =
+            AvaloniaProperty.RegisterAttached<TabControlAttach, Control, Thickness>("HeaderBorderThickness");
+
+        public static readonly AttachedProperty<Thickness> HeaderPaddingProperty =
+            AvaloniaProperty.RegisterAttached<TabControlAttach, Control, Thickness>("HeaderPadding");
+
         public static HorizontalAlignment GetHorizontalHeaderAlignment(Control obj) => obj.GetValue(HorizontalHeaderAlignmentProperty);
         public static void SetHorizontalHeaderAlignment(Control obj, HorizontalAlignment value) => obj.SetValue(HorizontalHeaderAlignmentProperty, value);
 
@@ -23,5 +32,14 @@ namespace Synthora.Attaches
 
         public static bool GetShowHeaderSeparator(Control obj) => obj.GetValue(ShowHeaderSeparatorProperty);
         public static void SetShowHeaderSeparator(Control obj, bool value) => obj.SetValue(ShowHeaderSeparatorProperty, value);
+
+        public static CornerRadius GetHeaderCornerRadius(Control obj) => obj.GetValue(HeaderCornerRadiusProperty);
+        public static void SetHeaderCornerRadius(Control obj, CornerRadius value) => obj.SetValue(HeaderCornerRadiusProperty, value);
+
+        public static Thickness GetHeaderBorderThickness(Control obj) => obj.GetValue(HeaderBorderThicknessProperty);
+        public static void SetHeaderBorderThickness(Control obj, Thickness value) => obj.SetValue(HeaderBorderThicknessProperty, value);
+
+        public static Thickness GetHeaderPadding(Control obj) => obj.GetValue(HeaderPaddingProperty);
+        public static void SetHeaderPadding(Control obj, Thickness value) => obj.SetValue(HeaderPaddingProperty, value);
     }
 }
