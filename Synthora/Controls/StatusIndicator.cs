b86f34308e55c9815f9b2cc37f8a8daa094615af
Yoyo <zrt2399@163.com>
2026-06-29 03:00:40 +0800

    feat: enhance StatusIndicator

--- Synthora/Controls/StatusIndicator.cs
+++ Synthora/Controls/StatusIndicator.cs
@@ -9,7 +9,7 @@ namespace Synthora.Controls
     /// Represents a compact status indicator.
     /// </summary>
     [PseudoClasses(pcNone, pcInformation, pcQuestion, pcSuccess, pcWarning, pcError)]
-    public class StatusIndicator : IconBase
+    public class StatusIndicator : ContentControl
     {
         private const string pcNone = ":none";
         private const string pcInformation = ":information";
@@ -19,14 +19,80 @@ namespace Synthora.Controls
         private const string pcError = ":error";
 
         /// <summary>
+        /// Defines the <see cref="IconType"/> property.
+        /// </summary>
+        public static readonly StyledProperty<IconType> IconTypeProperty =
+            AvaloniaProperty.Register<StatusIndicator, IconType>(nameof(IconType));
+
+        /// <summary>
+        /// Defines the <see cref="IconPlacement"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Dock> IconPlacementProperty =
+            AvaloniaProperty.Register<StatusIndicator, Dock>(nameof(IconPlacement));
+
+        /// <summary>
+        /// Defines the <see cref="IconWidth"/> property.
+        /// </summary>
+        public static readonly StyledProperty<double> IconWidthProperty =
+            AvaloniaProperty.Register<StatusIndicator, double>(nameof(IconWidth), 20d);
+
+        /// <summary>
+        /// Defines the <see cref="IconHeight"/> property.
+        /// </summary>
+        public static readonly StyledProperty<double> IconHeightProperty =
+            AvaloniaProperty.Register<StatusIndicator, double>(nameof(IconHeight), 20d);
+
+        /// <summary>
         /// Defines the <see cref="StrokeThickness"/> property.
         /// </summary>
         public static readonly StyledProperty<double> StrokeThicknessProperty =
             AvaloniaProperty.Register<StatusIndicator, double>(nameof(StrokeThickness));
 
+        /// <summary>
+        /// Defines the <see cref="HighlightMargin"/> property.
+        /// </summary>
+        public static readonly StyledProperty<Thickness> HighlightMarginProperty =
+            AvaloniaProperty.Register<StatusIndicator, Thickness>(nameof(HighlightMargin));
+
         static StatusIndicator()
-        { 
+        {
             IconTypeProperty.Changed.AddClassHandler<StatusIndicator, IconType>((s, e) => s.UpdatePseudoClasses());
+        }
+
+        /// <summary>
+        /// Gets or sets the status type.
+        /// </summary>
+        public IconType IconType
+        {
+            get => GetValue(IconTypeProperty);
+            set => SetValue(IconTypeProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the placement of the status icon.
+        /// </summary>
+        public Dock IconPlacement
+        {
+            get => GetValue(IconPlacementProperty);
+            set => SetValue(IconPlacementProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the width of the status icon.
+        /// </summary>
+        public double IconWidth
+        {
+            get => GetValue(IconWidthProperty);
+            set => SetValue(IconWidthProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the height of the status icon.
+        /// </summary>
+        public double IconHeight
+        {
+            get => GetValue(IconHeightProperty);
+            set => SetValue(IconHeightProperty, value);
         }
 
         /// <summary>
@@ -36,6 +102,15 @@ namespace Synthora.Controls
         {
             get => GetValue(StrokeThicknessProperty);
             set => SetValue(StrokeThicknessProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the margin of the status highlight.
+        /// </summary>
+        public Thickness HighlightMargin
+        {
+            get => GetValue(HighlightMarginProperty);
+            set => SetValue(HighlightMarginProperty, value);
         }
 
         /// <inheritdoc/>
