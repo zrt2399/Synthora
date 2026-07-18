d52d6b97b011067126de18ece7f37668a7fd16d9
Yoyo <zrt2399@163.com>
2026-07-18 17:50:22 +0800

    refactor: rename EnableDropDownIconAnimation to DropDownIconAnimationEnabled

--- Synthora/Attaches/DropDownButtonAttach.cs
+++ Synthora/Attaches/DropDownButtonAttach.cs
@@ -8,13 +8,13 @@ namespace Synthora.Attaches
         public static readonly AttachedProperty<object?> DropDownIconProperty =
             AvaloniaProperty.RegisterAttached<DropDownButtonAttach, Control, object?>("DropDownIcon");
 
-        public static readonly AttachedProperty<bool> EnableDropDownIconAnimationProperty =
-            AvaloniaProperty.RegisterAttached<DropDownButtonAttach, Control, bool>("EnableDropDownIconAnimation", defaultValue: true);
+        public static readonly AttachedProperty<bool> DropDownIconAnimationEnabledProperty =
+            AvaloniaProperty.RegisterAttached<DropDownButtonAttach, Control, bool>("DropDownIconAnimationEnabled", defaultValue: true);
 
         public static object? GetDropDownIcon(Control obj) => obj.GetValue(DropDownIconProperty);
         public static void SetDropDownIcon(Control obj, object? value) => obj.SetValue(DropDownIconProperty, value);
 
-        public static bool GetEnableDropDownIconAnimation(Control obj) => obj.GetValue(EnableDropDownIconAnimationProperty);
-        public static void SetEnableDropDownIconAnimation(Control obj, bool value) => obj.SetValue(EnableDropDownIconAnimationProperty, value);
+        public static bool GetDropDownIconAnimationEnabled(Control obj) => obj.GetValue(DropDownIconAnimationEnabledProperty);
+        public static void SetDropDownIconAnimationEnabled(Control obj, bool value) => obj.SetValue(DropDownIconAnimationEnabledProperty, value);
     }
 }
